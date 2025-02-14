c39fdbd31a1d1bc1f1bceeed1ad44ec26f74e362
elliot-choi-booth <elliotc0@chicagobooth.edu>
2025-02-14 21:08:13 +0000

    initial commit

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,22 @@
+class DiceController < ApplicationController
+  def two_six
+    render({ :template => "dice_templates/2d6"})
+  end
+
+  def two_ten
+    render({ :template => "dice_templates/2d10"})
+  end
+
+  def one_twenty
+    render({ :template => "dice_templates/1d20"})
+  end
+
+  def five_twenty
+    render({ :template => "dice_templates/5d20"})
+  end
+
+  def home
+    render({ :template => "dice_templates/home"})
+  end
+
+end
